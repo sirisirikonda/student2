5dea5cc780fcb2f667f85c9b72f0823cc66ce711
sirisirikonda <31827682+sirisirikonda@users.noreply.github.com>
2017-09-11 13:55:00 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+	    StudentGroup stdgrp = new StudentGroup(1);
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+	}
+
+}
